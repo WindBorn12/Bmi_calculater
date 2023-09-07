94b608660f7d6767787315c3a20589ec5013eb06
Rüzgar Duğan <windborn12@gmail.com>
2023-09-07 20:40:06 +0300

    error solved

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+import os
+import sys
 from tkinter import *
 
 
 def button_click():
-    weight = int(weight_text.get())
-    height = int(height_text.get())
-    height_m = height/100
-    Bmi = int(weight/(height_m**2))
-    print(weight)
-    print(height)
-    print(height_m)
-    print(Bmi)
-    if Bmi < 19:
-        Bmi_label.config(text="You are Underweight")
-    if Bmi >=19 and Bmi < 25:
-        Bmi_label.config(text="You are Normal")
-    if Bmi >=25 and Bmi < 40:
-        Bmi_label.config(text="You are OverWeight")
-    if Bmi > 40:
-        Bmi_label.config(text="You are Obese")
-    Bmi_label.pack()
+    try:
+        weight = int(weight_text.get())
+        height = int(height_text.get())
+        height_m = height/100
+        Bmi = int(weight/(height_m**2))
+        print(weight)
+        print(height)
+        print(height_m)
+        print(Bmi)
+
+        if Bmi < 19:
+            Bmi_label.config(text="You are Underweight")
+        if Bmi >=19 and Bmi < 25:
+            Bmi_label.config(text="You are Normal")
+        if Bmi >=25 and Bmi < 40:
+            Bmi_label.config(text="You are OverWeight")
+        if Bmi > 40:
+            Bmi_label.config(text="You are Obese")
+        Bmi_label.pack()
+    except:
+        Bmi_label.config(text="Enter a valid number!")
+
 
 
 
